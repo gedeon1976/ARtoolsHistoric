6a55536d9cc1f106af31700e6b3d4e41612923c1
portilla <portilla@77b821d9-fe1a-0410-914c-980f3157e59f>
2008-08-06 17:08:15 +0000

    updated to last modules to find coin and Qt4 libraries under cmake
    git-svn-id: svn+ssh://ris.upc.es/svnroot/ARtools/RTSPClient/branches/Tarraco@65 77b821d9-fe1a-0410-914c-980f3157e59f

--- Modules/FindCoin.cmake
+++ Modules/FindCoin.cmake
@@ -6,22 +6,19 @@ FIND_PATH(COIN_INCLUDE_DIR Inventor/So.h
     /usr/local/include
     /usr/include
     $ENV{ProgramFiles}/Coin3D-2/include
+	$ENV{COINDIR}/include
 )
 
-IF (NOT COIN_INCLUDE_DIR_FOUND)
-    IF (COIN_INCLUDE_DIR)
-        MESSAGE(STATUS "Looking for Coin3D headers -- found " ${COIN_INCLUDE_DIR}/Inventor/So.h)
-        SET(COIN_INCLUDE_DIR_FOUND 1 CACHE INTERNAL "Coin3D headers found")
-    ELSE (COIN_INCLUDE_DIR)
-        MESSAGE(FATAL_ERROR 
-            "-- Looking for Coin3D headers -- not found\n"
-            "Please install Coin3D http://www.coin3d.org/ or adjust CMAKE_INCLUDE_PATH\n"
-            "e.g. cmake -DCMAKE_INCLUDE_PATH=/path-to-Coin/include ..."
-        )
-    ENDIF (COIN_INCLUDE_DIR)
-ENDIF (NOT COIN_INCLUDE_DIR_FOUND)
+IF(COIN_INCLUDE_DIR)
+    MESSAGE(STATUS "Looking for Coin3D headers -- found " ${COIN_INCLUDE_DIR}/Inventor/So.h)
+ELSE(COIN_INCLUDE_DIR)
+MESSAGE(SEND_ERROR 
+     "Looking for Coin3D headers -- not found\n"
+     "Please install Coin3D http://www.coin3d.org/ or adjust CMAKE_INCLUDE_PATH"
+     "e.g. cmake -DCMAKE_INCLUDE_PATH=/path-to-Coin/include ...")
+ENDIF (COIN_INCLUDE_DIR)
 
-FIND_LIBRARY(COIN_LIBRARY
+FIND_LIBRARY(COIN_LIBRARY_RELEASE
     NAMES Coin coin2
     PATHS
     ${CMAKE_LIBRARY_PATH}
@@ -30,129 +27,70 @@ FIND_LIBRARY(COIN_LIBRARY
     /usr/local/lib
     /usr/lib
     $ENV{ProgramFiles}/Coin3D-2/lib
+	$ENV{COINDIR}/lib
 )
 
-IF (NOT COIN_LIBRARY_FOUND)
-    IF (COIN_LIBRARY)
-        MESSAGE(STATUS "Looking for Coin3D library -- found " ${COIN_LIBRARY})
-        SET(COIN_LIBRARY_FOUND 1 CACHE INTERNAL "Coin3D library found")
-    ELSE (COIN_LIBRARY)
-        MESSAGE(FATAL_ERROR 
-            "-- Looking for Coin3D library -- not found\n"
-            "Please install Coin3D http://www.coin3d.org/ or adjust CMAKE_LIBRARY_PATH\n"
-            "e.g. cmake -DCMAKE_LIBRARY_PATH=/path-to-Coin/lib ..."
-        )
-    ENDIF (COIN_LIBRARY)
-ENDIF (NOT COIN_LIBRARY_FOUND)
+FIND_LIBRARY(COIN_LIBRARY_DEBUG
+    NAMES Coind coin2d 
+    PATHS
+    ${CMAKE_LIBRARY_PATH}
+    $ENV{COIN3DDIR}/lib
+    /usr/local/coin/lib
+    /usr/local/lib
+    /usr/lib/debug
+    $ENV{ProgramFiles}/Coin3D-2/lib
+    $ENV{COINDIR}/lib
+)
+
+IF (COIN_LIBRARY_RELEASE)
+    MESSAGE(STATUS "Looking for Coin3D library -- found " ${COIN_LIBRARY_RELEASE})
+ELSE (COIN_LIBRARY_RELEASE)
+    MESSAGE(SEND_ERROR 
+    "Looking for Coin3D library -- not found"
+    "Please install Coin3D http://www.coin3d.org/ or adjust CMAKE_LIBRARY_PATH"
+    "e.g. cmake -DCMAKE_LIBRARY_PATH=/path-to-Coin/lib ..."
+    "or try COIN3DDIR with the correct value")
+ENDIF (COIN_LIBRARY_RELEASE)
+
+IF (COIN_LIBRARY_DEBUG)
+    MESSAGE(STATUS "Looking for Coin3D library debug version-- found " ${COIN_LIBRARY_DEBUG})
+ELSE (COIN_LIBRARY_DEBUG)
+    MESSAGE(STATUS 
+    "Looking for Coin3D library with debug symbols -- not found")
+ENDIF (COIN_LIBRARY_DEBUG)
+
+
+IF (COIN_LIBRARY_DEBUG AND COIN_LIBRARY_RELEASE)
+    # if the generator supports configuration types then set
+    # optimized and debug libraries, or if the CMAKE_BUILD_TYPE has a value
+    IF (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+      SET(COIN_LIBRARY optimized ${COIN_LIBRARY_RELEASE} debug ${COIN_LIBRARY_DEBUG})
+    ELSE(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+      # if there are no configuration types and CMAKE_BUILD_TYPE has no value
+      # then just use the release libraries
+      SET(COIN_LIBRARY ${COIN_LIBRARY_RELEASE} )
+    ENDIF(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    
+    SET(COIN_LIBRARIES optimized ${COIN_LIBRARY_RELEASE} debug ${COIN_LIBRARY_DEBUG})
+ELSE(COIN_LIBRARY_DEBUG AND COIN_LIBRARY_RELEASE)
+    # if have some lib or nothing
+    IF(COIN_LIBRARY_DEBUG)
+       SET(COIN_LIBRARY ${COIN_LIBRARY_DEBUG})
+    ENDIF(COIN_LIBRARY_DEBUG)
+    IF(COIN_LIBRARY_RELEASE)
+        SET(COIN_LIBRARY ${COIN_LIBRARY_RELEASE})
+    ENDIF(COIN_LIBRARY_RELEASE)
+ENDIF (COIN_LIBRARY_DEBUG AND COIN_LIBRARY_RELEASE)
+
+#SET(COIN_LIBRARY_FOUND 1 CACHE INTERNAL "Coin3D library found")
+IF(COIN_INCLUDE_DIR AND COIN_LIBRARY)
+    SET(COIN_LIBRARY_FOUND 1 CACHE INTERNAL "Coin3D library found to develop")
+ENDIF(COIN_INCLUDE_DIR AND COIN_LIBRARY)
 
 MARK_AS_ADVANCED(
+    COIN_LIBRARY_FOUND
     COIN_INCLUDE_DIR
     COIN_LIBRARY
+    COIN_LIBRARY_RELEASE
+    COIN_LIBRARY_DEBUG
 ) 
-# 
-# 
-# #############################################################################
-# #
-# # $Id: FindCOIN.cmake,v 1.4 2007/03/22 09:24:04 asaunier Exp $
-# #
-# # Copyright (C) 1998-2006 Inria. All rights reserved.
-# #
-# # This software was developed at:
-# # IRISA/INRIA Rennes
-# # Projet Lagadic
-# # Campus Universitaire de Beaulieu
-# # 35042 Rennes Cedex
-# # http://www.irisa.fr/lagadic
-# #
-# # This file is part of the ViSP toolkit
-# #
-# # This file may be distributed under the terms of the Q Public License
-# # as defined by Trolltech AS of Norway and appearing in the file
-# # LICENSE included in the packaging of this file.
-# #
-# # Licensees holding valid ViSP Professional Edition licenses may
-# # use this file in accordance with the ViSP Commercial License
-# # Agreement provided with the Software.
-# #
-# # This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-# # WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-# #
-# # Contact visp@irisa.fr if any conditions of this licensing are
-# # not clear to you.
-# #
-# # Description:
-# # Try to find Coin library .
-# # Once run this will define: 
-# #
-# # COIN_FOUND
-# # COIN_LIBRARIES
-# # COIN_LIBRARY_DEBUG
-# # COIN_LIBRARY_RELEASE
-# #
-# # Authors:
-# # Fabien Spindler
-# #
-# #############################################################################
-# 
-# IF(UNIX OR WIN32) 
-#   
-# IF(WIN32)
-#  FIND_LIBRARY(COIN_LIBRARY_RELEASE
-#     NAMES coin2 #only shared libraries under windows
-#     PATHS
-#      "$ENV{COINDIR}/lib"	  
-#     )
-# 
-#  FIND_LIBRARY(COIN_LIBRARY_DEBUG
-#     NAMES coin2d #only shared libraries under windows
-#     PATHS
-#     "$ENV{COINDIR}/lib"	  
-#     )
-#  MARK_AS_ADVANCED(
-#       COIN_LIBRARY_RELEASE
-#       COIN_LIBRARY_DEBUG
-#  )
-# 
-# ELSE(WIN32)
-#   FIND_LIBRARY(COIN_LIBRARY
-#     NAMES Coin #only shared libraries under windows
-#     PATHS
-#      "$ENV{COINDIR}/lib"	  
-#     )
-#   
-#   #MESSAGE(STATUS "DBG COIN_LIBRARY=${COIN_LIBRARY}")
-# ENDIF(WIN32)
-#   
-#   ## --------------------------------
-#   
-#   IF(COIN_LIBRARY OR COIN_LIBRARY_RELEASE OR COIN_LIBRARY_DEBUG)
-#     	IF(WIN32)
-# 	 	IF(COIN_LIBRARY_RELEASE AND NOT COIN_LIBRARY_DEBUG)
-# 			 SET(COIN_LIBRARY_RELEASE ${COIN_LIBRARY_RELEASE})			 
-# 		ENDIF(COIN_LIBRARY_RELEASE AND NOT COIN_LIBRARY_DEBUG)
-# 		IF(COIN_LIBRARY_DEBUG AND NOT COIN_LIBRARY_RELEASE)
-# 			 SET(COIN_LIBRARY_DEBUG ${COIN_LIBRARY_DEBUG})			 
-# 		ENDIF(COIN_LIBRARY_DEBUG AND NOT COIN_LIBRARY_RELEASE)
-# 		IF(COIN_LIBRARY_RELEASE AND COIN_LIBRARY_DEBUG)
-# 			 SET(COIN_LIBRARY_RELEASE ${COIN_LIBRARY_RELEASE})
-# 			 SET(COIN_LIBRARY_DEBUG ${COIN_LIBRARY_DEBUG})			
-# 		ENDIF(COIN_LIBRARY_RELEASE AND COIN_LIBRARY_DEBUG)
-# 	ELSE(WIN32)
-# 		SET(COIN_LIBRARIES ${COIN_LIBRARY})
-# 	    MARK_AS_ADVANCED(
-# 	        COIN_LIBRARIES
-#      		COIN_LIBRARY
-#     	    )
-# 
-# 	ENDIF(WIN32)
-#     SET(COIN_FOUND TRUE)
-#   ELSE(COIN_LIBRARY OR COIN_LIBRARY_RELEASE OR COIN_LIBRARY_DEBUG)
-#     SET(COIN_FOUND FALSE)
-#     #MESSAGE("Coin library not found.")
-#   ENDIF(COIN_LIBRARY OR COIN_LIBRARY_RELEASE OR COIN_LIBRARY_DEBUG)
-# 
-#   #MESSAGE(STATUS "COIN_FOUND : ${COIN_FOUND}")
-# 
-# ELSE(UNIX OR WIN32)
-# 	SET(COIN_FOUND FALSE)
-# ENDIF(UNIX OR WIN32) 
--- Modules/FindSoQt4.cmake
+++ Modules/FindSoQt4.cmake
@@ -1,54 +1,103 @@
-FIND_PATH(SOQT_INCLUDE_DIR Inventor/Qt4/SoQt.h
-        ${CMAKE_INCLUDE_PATH}
-        $ENV{COIN3DDIR}/include
-        /usr/local/soqt/include
-        /usr/local/SoQt/include
-        /usr/local/include
-        /usr/include
-        $ENV{ProgramFiles}/SoQt-1/include
-        $ENV{ProgramFiles}/Coin3D-2/include
+FIND_PATH(SOQT_INCLUDE_DIR Inventor/Qt/SoQt.h
+	${CMAKE_INCLUDE_PATH}
+	$ENV{COIN3DDIR}/include
+	/usr/local/soqt/include
+	/usr/local/SoQt/include
+	/usr/local/include
+	/usr/include
+	$ENV{ProgramFiles}/SoQt-1/include
+	$ENV{ProgramFiles}/Coin3D-2/include
+	$ENV{COINDIR}/include
 )
 
-IF (NOT SOQT_INCLUDE_DIR_FOUND)
-        IF (SOQT_INCLUDE_DIR)
-                MESSAGE(STATUS "Looking for SoQt headers -- found " ${SOQT_INCLUDE_DIR}/Inventor/Qt4/SoQt.h)
-                SET(SOQT_INCLUDE_DIR_FOUND 1 CACHE INTERNAL "SoQt headers found")
-        ELSE (SOQT_INCLUDE_DIR)
-                MESSAGE(FATAL_ERROR 
-                        "-- Looking for SoQt headers -- not found\n"
-                        "Please install SoQt http://www.coin3d.org/ or adjust CMAKE_INCLUDE_PATH\n"
-                        "e.g. cmake -DCMAKE_INCLUDE_PATH=/path-to-SoQt/include ..."
-                )
-        ENDIF (SOQT_INCLUDE_DIR)
-ENDIF (NOT SOQT_INCLUDE_DIR_FOUND)
+IF (SOQT_INCLUDE_DIR)
+	MESSAGE(STATUS "Looking for SoQt headers -- found " ${SOQT_INCLUDE_DIR}/Inventor/Qt/SoQt.h)
+		SET(SOQT_INCLUDE_DIR_FOUND 1 CACHE INTERNAL "SoQt headers found")
+ELSE (SOQT_INCLUDE_DIR)
+	MESSAGE(SEND_ERROR 
+	"Looking for SoQt headers -- not found"
+	"Please install SoQt http://www.coin3d.org/ or adjust CMAKE_INCLUDE_PATH"
+	"e.g. cmake -DCMAKE_INCLUDE_PATH=/path-to-SoQt/include ...")
+ENDIF (SOQT_INCLUDE_DIR)
 
-FIND_LIBRARY(SOQT_LIBRARY
-        NAMES SoQt4 soqt1
-        PATHS
-        ${CMAKE_LIBRARY_PATH}
-        $ENV{COIN3DDIR}/lib
-        /usr/local/soqt/lib
-        /usr/local/SoQt/lib
-        /usr/local/lib
-        /usr/lib
-        $ENV{ProgramFiles}/SoQt-1/lib
-        $ENV{ProgramFiles}/Coin3D-2/lib
+
+FIND_LIBRARY(SOQT_LIBRARY_RELEASE
+	NAMES SoQt4 soqt1 SoQt
+	PATHS
+	${CMAKE_LIBRARY_PATH}
+	$ENV{COIN3DDIR}/lib
+	/usr/local/soqt/lib
+	/usr/local/SoQt/lib
+	/usr/local/lib
+	/usr/lib
+	$ENV{ProgramFiles}/SoQt-1/lib
+	$ENV{ProgramFiles}/Coin3D-2/lib
+	$ENV{COINDIR}/lib
 )
 
-IF (NOT SOQT_LIBRARY_FOUND)
-        IF (SOQT_LIBRARY)
-                MESSAGE(STATUS "Looking for SoQt library -- found " ${SOQT_LIBRARY})
-                SET(SOQT_LIBRARY_FOUND 1 CACHE INTERNAL "SoQt library found")
-        ELSE (SOQT_LIBRARY)
-                MESSAGE(FATAL_ERROR 
-                        "-- Looking for SoQt library -- not found\n"
-                        "Please install SoQt http://www.coin3d.org/ or adjust CMAKE_LIBRARY_PATH\n"
-                        "e.g. cmake -DCMAKE_LIBRARY_PATH=/path-to-SoQt/lib ..."
-                )
-        ENDIF (SOQT_LIBRARY)
-ENDIF (NOT SOQT_LIBRARY_FOUND)
+FIND_LIBRARY(SOQT_LIBRARY_DEBUG
+    NAMES SoQt4d SoQtd soqt1d 
+    PATHS
+    ${CMAKE_LIBRARY_PATH}
+    $ENV{COIN3DDIR}/lib
+    /usr/local/soqt/lib
+    /usr/local/SoQt/lib
+    /usr/local/lib
+    /usr/lib/debug
+    $ENV{ProgramFiles}/SoQt-1/lib
+    $ENV{ProgramFiles}/Coin3D-2/lib
+    $ENV{COINDIR}/lib
+)
+
+IF (SOQT_LIBRARY_RELEASE)
+	MESSAGE(STATUS "Looking for SoQt library -- found " ${SOQT_LIBRARY_RELEASE})
+ELSE (SOQT_LIBRARY_RELEASE)
+ 	MESSAGE(SENDL_ERROR 
+	"Looking for SoQt library -- not found"
+	"Please install SoQt http://www.coin3d.org/ or adjust CMAKE_LIBRARY_PATH"
+	"e.g. cmake -DCMAKE_LIBRARY_PATH=/path-to-SoQt/lib ..."
+    "or try COIN3DDIR with the correct value")
+ENDIF (SOQT_LIBRARY_RELEASE)
+
+IF (SOQT_LIBRARY_DEBUG)
+    MESSAGE(STATUS "Looking for SoQt library debub -- found " ${SOQT_LIBRARY_DEBUG})
+ELSE (SOQT_LIBRARY_DEBUG)
+    MESSAGE(STATUS 
+    "Looking for SoQt library debug -- not found")
+ENDIF (SOQT_LIBRARY_DEBUG)
+
+
+IF (SOQT_LIBRARY_DEBUG AND SOQT_LIBRARY_RELEASE)
+    # if the generator supports configuration types then set
+    # optimized and debug libraries, or if the CMAKE_BUILD_TYPE has a value
+    IF (CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+      SET(SOQT_LIBRARY optimized ${SOQT_LIBRARY_RELEASE} debug ${SOQT_LIBRARY_DEBUG})
+    ELSE(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+      # if there are no configuration types and CMAKE_BUILD_TYPE has no value
+      # then just use the release libraries
+      SET(SOQT_LIBRARY ${SOQT_LIBRARY_RELEASE} )
+    ENDIF(CMAKE_CONFIGURATION_TYPES OR CMAKE_BUILD_TYPE)
+    
+    SET(SOQT_LIBRARIES optimized ${SOQT_LIBRARY_RELEASE} debug ${SOQT_LIBRARY_DEBUG})
+ELSE(SOQT_LIBRARY_DEBUG AND SOQT_LIBRARY_RELEASE)
+    # if have some lib or nothing
+    IF(SOQT_LIBRARY_DEBUG)
+       SET(SOQT_LIBRARY ${SOQT_LIBRARY_DEBUG})
+    ENDIF(SOQT_LIBRARY_DEBUG)
+    IF(SOQT_LIBRARY_RELEASE)
+        SET(SOQT_LIBRARY ${SOQT_LIBRARY_RELEASE})
+    ENDIF(SOQT_LIBRARY_RELEASE)
+ENDIF (SOQT_LIBRARY_DEBUG AND SOQT_LIBRARY_RELEASE)
+
+#SET(SOQT_LIBRARY_FOUND 1 CACHE INTERNAL "Coin3D library found")
+IF(SOQT_INCLUDE_DIR AND SOQT_LIBRARY)
+    SET(SOQT_LIBRARY_FOUND 1 CACHE INTERNAL "Coin3D library found to develop")
+ENDIF(SOQT_INCLUDE_DIR AND SOQT_LIBRARY)
 
 MARK_AS_ADVANCED(
-        SOQT_INCLUDE_DIR
-        SOQT_LIBRARY
-)  
+    SOQT_LIBRARY_FOUND
+    SOQT_INCLUDE_DIR
+    SOQT_LIBRARY
+    SOQT_LIBRARY_RELEASE
+    SOQT_LIBRARY_DEBUG
+) 
